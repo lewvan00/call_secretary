39fd286285bd6114f51ae733f59c640ea737dc5d
lidong3 <lidong3@cmcm.com>
2017-03-30 16:02:31 +0800

    修改悬浮窗位置，加入触摸移动

--- CallSecretary/app/src/main/java/call/ai/com/callsecretary/FloatingWindowsService.java
+++ CallSecretary/app/src/main/java/call/ai/com/callsecretary/FloatingWindowsService.java
@@ -6,12 +6,12 @@ import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -29,6 +29,8 @@ public class FloatingWindowsService extends Service {
     TextView mDetailTv;
     Button mCloseBtn;
     boolean hasFloatingShowing = false;
+    float mOffsetX;
+    float mOffsetY;
 
 
     @Nullable
@@ -52,9 +54,12 @@ public class FloatingWindowsService extends Service {
 
         mLayoutParams.type = WindowManager.LayoutParams.TYPE_PHONE;
         mLayoutParams.format = PixelFormat.RGBA_8888;
-        mLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
-        mLayoutParams.gravity = Gravity.CENTER_HORIZONTAL;
+        mLayoutParams.gravity = Gravity.TOP | Gravity.LEFT;
 
+        mLayoutParams.x=mWindowManager.getDefaultDisplay().getWidth()/6;
+        mLayoutParams.y=mWindowManager.getDefaultDisplay().getHeight()/3;
+
+        mLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
         mLayoutParams.width = mWindowManager.getDefaultDisplay().getWidth() * 2 / 3;
         mLayoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
     }
@@ -69,6 +74,28 @@ public class FloatingWindowsService extends Service {
                 @Override
                 public void onClick(View view) {
                     hideFloatingWindows();
+                }
+            });
+
+            mTitleTv.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    int action = motionEvent.getAction();
+                    switch (action) {
+                        case MotionEvent.ACTION_DOWN:
+                            mOffsetX = motionEvent.getRawX() - mLayoutParams.x;
+                            mOffsetY = motionEvent.getRawY() - mLayoutParams.y;
+                            break;
+                        case MotionEvent.ACTION_MOVE:
+                            mLayoutParams.x = (int) (motionEvent.getRawX() - mOffsetX);
+                            mLayoutParams.y = (int) (motionEvent.getRawY() - mOffsetY);
+                            mWindowManager.updateViewLayout(mFloatingView, mLayoutParams);
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            break;
+                    }
+
+                    return true;
                 }
             });
         }
