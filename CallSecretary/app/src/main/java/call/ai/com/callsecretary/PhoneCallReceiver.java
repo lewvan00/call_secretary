cc6824a845eb6954958c58726fbea900c87aedd6
lidong <hbutxld@gmail.com>
2017-04-07 23:27:16 +0800

    删除部分无用代码和Toast

--- CallSecretary/app/src/main/java/call/ai/com/callsecretary/PhoneCallReceiver.java
+++ CallSecretary/app/src/main/java/call/ai/com/callsecretary/PhoneCallReceiver.java
@@ -19,33 +19,23 @@ import call.ai.com.callsecretary.utils.PhoneUtils;
 
 public class PhoneCallReceiver extends BroadcastReceiver {
 
-//    private CallRecorder mCallRecorder;
     private FloatingWindowsService mFloatingService;
 
     public PhoneCallReceiver() {
-//        mCallRecorder = new CallRecorder();
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent != null && intent.getAction().equals("android.intent.action.PHONE_STATE")) {
             TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-            Log.d("liufan", "action = " + intent.getAction() + ", state = " + telephonyManager.getCallState());
-            Toast.makeText(context, "action = " + intent.getAction() + ", state = " + telephonyManager.getCallState(), Toast.LENGTH_LONG).show();
             if (telephonyManager.getCallState() == TelephonyManager.CALL_STATE_RINGING) {
-                //来电
                 PhoneUtils.autoAnswer();
-//                PhoneUtils.setSpeekModle(true);   //开启外放
                 mFloatingService = new FloatingWindowsService();
                 mFloatingService.showFloatingWindows("liufan");
-                Toast.makeText(CallSecretaryApplication.getContext(), "starting bot ", Toast.LENGTH_LONG).show();
                 mFloatingService.startBot();
-//                mCallRecorder.startRecording();
             } else if (telephonyManager.getCallState() == TelephonyManager.CALL_STATE_IDLE) {
-                //挂断
                 PhoneUtils.setSpeekModle(false);   //关闭外放
-//                mCallRecorder.stopRecoding();
-            }else if (telephonyManager.getCallState()==TelephonyManager.CALL_STATE_OFFHOOK){
+            } else if (telephonyManager.getCallState() == TelephonyManager.CALL_STATE_OFFHOOK) {
                 PhoneUtils.setSpeekModle(true);
             }
         }
