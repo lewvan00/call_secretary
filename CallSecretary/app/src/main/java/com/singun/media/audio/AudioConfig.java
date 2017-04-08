bbbae511552e409f505d8e25238c6a2074563a27
singun-lxd <liuxindian@gmail.com>
2017-04-08 13:19:01 +0800

    * 录音写到文件里

---
+++ CallSecretary/app/src/main/java/com/singun/media/audio/AudioConfig.java
@@ -0,0 +1,21 @@
+package com.singun.media.audio;
+
+/**
+ * Created by singun on 2017/3/1 0001.
+ */
+
+public class AudioConfig {
+    public int streamType;
+    public int audioSource;
+    public int sampleRateInHz;
+    public int channelInConfig;
+    public int channelOutConfig;
+    public int audioFormat;
+
+    public int sessionId;
+    public int audioDataSize;
+    public short[] audioDataIn;
+    public short[] audioDataOut;
+    public String audioDirPath;
+    public String audioName;
+}
