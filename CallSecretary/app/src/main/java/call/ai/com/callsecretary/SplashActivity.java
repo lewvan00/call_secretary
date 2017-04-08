ce3a30fb32441fee1fd5b17ac9379cc8687c68d9
lizhiwei <wei2006004@foxmail.com>
2017-04-08 14:44:39 +0800

    添加splashactivity

---
+++ CallSecretary/app/src/main/java/call/ai/com/callsecretary/SplashActivity.java
@@ -0,0 +1,27 @@
+package call.ai.com.callsecretary;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_splash;
+    }
+}
